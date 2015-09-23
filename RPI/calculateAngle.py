9841eb6e46987986c165a893e7e17b99e02b62a0
taiyi123 <boo_tai_yi@hotmail.com>
2015-09-23 15:32:13 +0800

    updated calculate angle function

--- RPI/calculateAngle.py
+++ RPI/calculateAngle.py
@@ -1,3 +1,4 @@
+#Returns direction in degree
 def calculateAngle(currentX, currentY, nodeX, nodeY, northAt)
 	
 	#calculate distance
@@ -8,8 +9,8 @@ def calculateAngle(currentX, currentY, nodeX, nodeY, northAt)
 	
 	angleDeg = ((angleRad/math.pi) * 180)
 	
-	angleDegNorthAt = ((angleDeg + 360) %360)
+	angleDegPositive = ((angleDeg + 360) %360)
 	
-	direction = (angleDegNorthAt - northAt + 360) % 360
+	direction = (angleDegPositive - northAt + 360) % 360
 	
 	return direction
